0b1a401d9af051f30ae09f51fb4a10ddfb6bc2c7
Jordan <jhthewebguy94@gmail.com>
2022-07-02 23:27:16 -0700

    lastest pushes

--- install_everything.ps1
+++ install_everything.ps1
@@ -2,46 +2,77 @@ Install-Module -Name PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
 
 #Check for installed software
-function check_for_install ($software_list) {
-   $software = Get-CimInstance -ClassName Win32_OperatingSystem |  Where-Object { $_.IdentifyingNumber -like $software_list } 
+#function check_for_install ($software_list) {
+#  $software = Get-CimInstance -ClassName Win32_OperatingSystem |  Where-Object { $_.Name -like 'winget' }
+
+#}
+
+function show_menu {
+   param(
+      [string]$Title = "Install Everything"      
+   )
+   Clear-Host
+   Write-Host "======================= Title ======================="
+   Write-Host "1 -option Start the Script press '1' "
 }
+function download_winget {
+   $winget_link = @(      
+      'https://github.com/microsoft/winget-cli/releases/download/v1.3.1741/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'      
+   )      
+   $winget_link | ForEach-Object { Start-BitsTransfer $_ -Destination "C:\Windows\Temp" }
+}
+function install_winget {   
+   Start-Process "C:\Windows\Temps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"   
+}
+function office365_uninstallxml_download {   
+   $xml_files = @(          
+      "https://raw.githubusercontent.com/Jhman33/ps1scripts/main/uninstall.xml")
+   
+   $xml_files | ForEach-Object { Start-BitsTransfer $_ -Destination "C:\Program Files\OfficeDeploymentTool" }   
+} 
+
 #Get & install windows update
 function update_windows {
    Get-WindowsUpdate
    Install-WindowsUpdate -AcceptAll
 }
-#Install Milieu n-able
-#function install-n-able {
-#  start-Process -Wait -FilePath './milieu-n-able'
-#}
-#update-windows-store
 function update_windows_store {
-   Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod-MethodName UpdateScanMethod
-}
-function office365_install {   
-   $xml_files = @(
-      "configuration-Office365-x86.xml",          
-      "uninstall.xml"
-   )
-   Start-BitsTransfer "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_15225-20204.exe" -Destination "C:\Users\Public\Downloads"
-   foreach ($xml in $xml_files){
-      start-Process -FilePath "officedeploymenttool_15225-20204.exe" -Wait -PassThru -ArgumentList "/configure", $xml
-   }   
-}
+   Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+} 
+
 function install-programs {
    $winget_install_list = @(
-      "Google.Chrome"
-      "Adobe.Acrobat.Reader.64-bit"
-      "Foxit.FoxitReader"
-      "VideoLAN.VLC"
+      "Google.Chrome",
+      "Adobe.Acrobat.Reader.64-bit",
+      "Foxit.FoxitReader",
+      "VideoLAN.VLC",
+      "Microsoft.OfficeDeploymentTool"
    )
 
    foreach ($software in $winget_install_list) {
       winget install $software
    }
 }
+function install_office {
+   office365_uninstallxml_download  
+   $xml = @(
+      "uninstall.xml"
+      "configuration-Office365-x64.xml"
+   )     
+   $xml | ForEach-Object { start-Process -FilePath "setup.exe" -Wait -WorkingDirectory "C:\Program Files\OfficeDeploymentTool\" -ArgumentList "/configure $_" }
+}
 
-update_windows_store
-update_windows
-office365_install
-install-programs
+do {
+   show_menu
+   $selection = Read-Host "Please make a slection"
+   switch ($selection) {
+      '1' {
+         update_windows_store
+         update_windows            
+         install_winget
+         install-programs
+         install_office
+      } 
+   }
+   Pause
+} until ($selection -eq 'q')
