3be050611008c0b76f74335fb691992c4e81f70a
Jordan <jhthewebguy94@gmail.com>
2022-07-03 22:55:20 -0700

    more commits

--- install_everything.ps1
+++ install_everything.ps1
@@ -1,12 +1,9 @@
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
-
 #Check for installed software
 #function check_for_install ($software_list) {
 #  $software = Get-CimInstance -ClassName Win32_OperatingSystem |  Where-Object { $_.Name -like 'winget' }
-
 #}
-
 function show_menu {
    param(
       [string]$Title = "Install Everything"      
@@ -22,57 +19,51 @@ function download_winget {
    $winget_link | ForEach-Object { Start-BitsTransfer $_ -Destination "C:\Windows\Temp" }
 }
 function install_winget {   
-   Start-Process "C:\Windows\Temps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"   
+   Start-Process 'C:\Windows\Temps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'   
 }
 function office365_uninstallxml_download {   
    $xml_files = @(          
-      "https://raw.githubusercontent.com/Jhman33/ps1scripts/main/uninstall.xml")
-   
-   $xml_files | ForEach-Object { Start-BitsTransfer $_ -Destination "C:\Program Files\OfficeDeploymentTool" }   
+      'https://raw.githubusercontent.com/Jhman33/ps1scripts/main/uninstall.xml')   
+   $xml_files | ForEach-Object { Start-BitsTransfer $_ -Destination 'C:\Program Files\OfficeDeploymentTool' }   
 } 
-
 #Get & install windows update
 function update_windows {
    Get-WindowsUpdate
    Install-WindowsUpdate -AcceptAll
 }
 function update_windows_store {
-   Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+   Get-CimInstance -Namespace 'Root\cimv2\mdm\dmmap' -ClassName 'MDM_EnterpriseModernAppManagement_AppManagement01' | Invoke-CimMethod -MethodName UpdateScanMethod
 } 
-
 function install-programs {
    $winget_install_list = @(
-      "Google.Chrome",
-      "Adobe.Acrobat.Reader.64-bit",
-      "Foxit.FoxitReader",
-      "VideoLAN.VLC",
-      "Microsoft.OfficeDeploymentTool"
+      'Google.Chrome',
+      'Adobe.Acrobat.Reader.64-bit',
+      'Foxit.FoxitReader',
+      'VideoLAN.VLC',
+      'Microsoft.OfficeDeploymentTool'
    )
-
-   foreach ($software in $winget_install_list) {
-      winget install $software
-   }
+   $winget_install_list | ForEach-Object { winget install --silent $_ }
 }
 function install_office {
    office365_uninstallxml_download  
    $xml = @(
-      "uninstall.xml"
-      "configuration-Office365-x64.xml"
+      'uninstall.xml'
+      'configuration-Office365-x64.xml'
    )     
-   $xml | ForEach-Object { start-Process -FilePath "setup.exe" -Wait -WorkingDirectory "C:\Program Files\OfficeDeploymentTool\" -ArgumentList "/configure $_" }
+   $xml | ForEach-Object { start-Process -FilePath 'setup.exe' -Wait -WorkingDirectory 'C:\Program Files\OfficeDeploymentTool\' -ArgumentList '/configure $_' }
 }
-
 do {
    show_menu
-   $selection = Read-Host "Please make a slection"
+   $selection = Read-Host 'Please make a slection'
    switch ($selection) {
       '1' {
-         update_windows_store
-         update_windows            
-         install_winget
-         install-programs
-         install_office
+         # update_windows_store
+         # update_windows            
+         # install_winget
+         # install-programs
+         # install_office
+         # Restart-Computer
+         Write-Host "test"
       } 
-   }
-   Pause
-} until ($selection -eq 'q')
+   }   
+} until ($true) {}
